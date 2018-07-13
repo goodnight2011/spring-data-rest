6f033189d7eb95bc4ad0608a21a51baaa34afeed
ABusovikov <ABusovikov@IBS.ru>
2018-07-13 14:17:03 +0500

    workable with parameters

--- src/main/java/ru/vcki/data/repoes/PersonRepository.java
+++ src/main/java/ru/vcki/data/repoes/PersonRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.rest.core.annotation.RestResource;
 import ru.vcki.data.pojo.Person;
 
 public interface PersonRepository extends PagingAndSortingRepository<Person, Long> {
-    Person findByFirstName(@Param("name") String firstName);
+    Person findByFirstName( String firstName);
 }
